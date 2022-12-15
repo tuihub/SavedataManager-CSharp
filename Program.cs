79b02b47d4fcb3e73603e090200bc27eb97f5aa3
Tabing010102 <tabing010102@outlook.com>
2022-12-15 17:58:05 +0800

    update Program.cs & update project structure

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using CommandLine;
+using SavedataManager_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavedataManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(Run)
+                .WithNotParsed(HandleParseError);
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errs)
+        {
+            if (errs.IsVersion()) return;
+            if (errs.IsHelp()) return;
+        }
+
+        private static void Run(Options opts)
+        {
+            Console.WriteLine("run");
+        }
+    }
+}
