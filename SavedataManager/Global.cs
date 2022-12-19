43b0b5147ae867215391d0829653c0b7ee79aeb3
Tabing010102 <tabing010102@outlook.com>
2022-12-19 16:56:37 +0800

    add App.config

--- SavedataManager/Global.cs
+++ SavedataManager/Global.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,13 +10,13 @@ namespace SavedataManager
 {
     public static class Global
     {
+        public static readonly LogLevel LogLevel = Enum.Parse<LogLevel>(ConfigurationManager.AppSettings["logLevel"] ?? "INFO");
+
         public static readonly string ExecutionPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
         public static readonly string ExecutionFolderPath = Path.GetDirectoryName(ExecutionPath);
-        public static readonly string SavedataArchiveFolderName = "archives";
-        public static readonly string SavedataArchiveFolderPath = Path.Combine(ExecutionFolderPath, SavedataArchiveFolderName);
-        public static readonly string SavedataConfigFileName = "savedata_manager_config.json";
-
-        public static LogLevel LogLevel = LogLevel.DEBUG;
+        public static readonly string SavedataArchiveFolderName = ConfigurationManager.AppSettings["savedataArchiveFolderName"] ?? "archives";
+        public static readonly string SavedataArchiveFolderPath = ConfigurationManager.AppSettings["savedataArchiveFolderPath"] ?? Path.Combine(ExecutionFolderPath, SavedataArchiveFolderName);
+        public static readonly string SavedataConfigFileName = ConfigurationManager.AppSettings["savedataConfigFileName"] ?? "savedata_manager_config.json";
     }
     public enum LogLevel
     {
