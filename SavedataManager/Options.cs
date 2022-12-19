b240c33a48db3ab05cb2e68223fa9faa0d2793ba
Tabing010102 <tabing010102@outlook.com>
2022-12-19 17:05:27 +0800

    change app info & update options

--- SavedataManager/Options.cs
+++ SavedataManager/Options.cs
@@ -7,16 +7,16 @@ using CommandLine;
 
 namespace SavedataManager
 {
-    [Verb("store", aliases: new string[] { "s" })]
+    [Verb("store", aliases: new string[] { "s" }, HelpText = "Store app savedata to file.")]
     class StoreOptions
     {
-        [Option('d', "dir", Required = true, HelpText = "App base folder path to store savedata.")]
+        [Option('d', "dir", Default = ".", HelpText = "App base folder path to store savedata.")]
         public string DirPath { get; set; } = null!;
         [Option('n', "name", Required = true, HelpText = "App name.")]
         public string AppName { get; set; } = null!;
     }
 
-    [Verb("restore", aliases: new string[] { "rs" })]
+    [Verb("restore", aliases: new string[] { "rs" }, HelpText = "Restore app savedata from file.")]
     class RestoreOptions
     {
         [Option('f', "file", Required = true, HelpText = "Savedata file path to restore.")]
