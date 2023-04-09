083ac2f0db1cc1729e1d17a2a89dd86ec75054e2
Tabing010102 <tabing010102@outlook.com>
2023-04-09 20:40:39 +0800

    add global exception handler

--- SavedataManagerGui/Program.cs
+++ SavedataManagerGui/Program.cs
@@ -8,10 +8,19 @@ namespace SavedataManagerGui
         [STAThread]
         static void Main()
         {
+            // Global exception handling
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(GlobalHandler);
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new MainForm());
+        }
+
+        static void GlobalHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception? e = args.ExceptionObject as Exception;
+            MessageBox.Show(e != null ? e.Message : "Unknown", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
