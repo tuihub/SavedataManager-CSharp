d56035ffe0ba25b44e1a99d9c80f2ffa16b43911
Tabing010102 <tabing010102@outlook.com>
2023-12-11 17:17:45 +0800

    feat: add V2.1 config model

--- TuiHub.SavedataManagerLibrary/Models/V2_1/Config.cs
+++ TuiHub.SavedataManagerLibrary/Models/V2_1/Config.cs
@@ -3,10 +3,39 @@
     public class Config
     {
         public Platform Platform { get; set; }
-        public List<object>? Entries { get; set; }
+        public List<Entry>? Entries { get; set; }
     }
     public enum Platform
     {
         Windows
+    }
+
+    public class Entry
+    {
+        public long Id { get; set; }
+        public BaseDirMode BaseDirMode { get; set; }
+        public string BaseDir { get; set; } = string.Empty;
+        public List<FilePattern>? FilePatterns { get; set; }
+        public bool ClearBaseDirBeforeRestore { get; set; }
+    }
+
+    public enum BaseDirMode
+    {
+        GameRoot,
+        UserDocument,
+        UserProfile,
+        Absolute
+    }
+
+    public class FilePattern
+    {
+        public FilePatternType Type { get; set; }
+        public string Pattern { get; set; } = string.Empty;
+    }
+
+    public enum FilePatternType
+    {
+        Include,
+        Exclude
     }
 }
