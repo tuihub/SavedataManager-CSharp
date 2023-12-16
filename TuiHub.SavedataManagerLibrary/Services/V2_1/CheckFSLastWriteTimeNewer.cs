bcc6c26158b329dac1ae2bed6032a75347a02b4c
Tabing010102 <tabing010102@outlook.com>
2023-12-16 11:16:01 +0800

    chore: extract GetFSFilesFromEntry

--- TuiHub.SavedataManagerLibrary/Services/V2_1/CheckFSLastWriteTimeNewer.cs
+++ TuiHub.SavedataManagerLibrary/Services/V2_1/CheckFSLastWriteTimeNewer.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TuiHub.SavedataManagerLibrary.Models.V2_1;
+using TuiHub.SavedataManagerLibrary.Services.V2_1.Utils;
 using TuiHub.SavedataManagerLibrary.Utils;
 
 namespace TuiHub.SavedataManagerLibrary.Services.V2_1
@@ -37,16 +38,7 @@ namespace TuiHub.SavedataManagerLibrary.Services.V2_1
                     _logger?.LogWarning($"entry.FilePatterns is null or empty, entryBaseDir = {entryBaseDir}");
                     continue;
                 }
-                var files = new List<string>();
-                foreach (var filePattern in entry.FilePatterns.Where(e => e.Type == FilePatternType.Include))
-                    files.AddRange(Directory.EnumerateFiles(entryBaseDir, filePattern.Pattern, new EnumerationOptions
-                    {
-                        RecurseSubdirectories = true,
-                        MatchCasing = config.IsCaseSensitve ? MatchCasing.CaseSensitive : MatchCasing.CaseInsensitive
-                    }));
-                foreach (var filePattern in entry.FilePatterns.Where(e => e.Type == FilePatternType.Exclude))
-                    files.RemoveAll(e => Regex.IsMatch(e, $"^{filePattern.Pattern}$",
-                        config.IsCaseSensitve ? RegexOptions.None : RegexOptions.IgnoreCase));
+                List<string> files = FSUtil.GetFSFilesFromEntry(config, entry, entryBaseDir);
                 DateTime entryLastWriteTime;
                 if (!files.Any())
                 {
---
+++ TuiHub.SavedataManagerLibrary/Services/V2_1/Utils/FSUtil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TuiHub.SavedataManagerLibrary.Models.V2_1;
+
+namespace TuiHub.SavedataManagerLibrary.Services.V2_1.Utils
+{
+    public static class FSUtil
+    {
+        public static List<string> GetFSFilesFromEntry(Config config, Entry entry, string entryBaseDir)
+        {
+            var files = new List<string>();
+            foreach (var filePattern in entry.FilePatterns!.Where(e => e.Type == FilePatternType.Include))
+                files.AddRange(Directory.EnumerateFiles(entryBaseDir, filePattern.Pattern, new EnumerationOptions
+                {
+                    RecurseSubdirectories = true,
+                    MatchCasing = config.IsCaseSensitve ? MatchCasing.CaseSensitive : MatchCasing.CaseInsensitive
+                }));
+            foreach (var filePattern in entry.FilePatterns!.Where(e => e.Type == FilePatternType.Exclude))
+                files.RemoveAll(e => Regex.IsMatch(e, $"^{filePattern.Pattern}$",
+                    config.IsCaseSensitve ? RegexOptions.None : RegexOptions.IgnoreCase));
+            return files;
+        }
+    }
+}
