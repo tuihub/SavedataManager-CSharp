f5ac067ea792d280a85a010b93543cc204198d12
Tabing010102 <tabing010102@outlook.com>
2023-12-21 10:08:40 +0800

    chore: remove SavedataManager

--- SavedataManager/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-[assembly: log4net.Config.XmlConfigurator(ConfigFileExtension = "log4net", Watch = true)]
